a7c351d948f583d5202c8065cf9e5277a234e808
John Soatra <johnsoatra@gmail.com>
2021-08-21 07:38:31 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="App">
       <h1>សូមស្វាគមន៍មកកាន់គេហទំព័រ Soatra</h1>
       <p>គេហទំព័រនេះគឺកំពុងអភិវឌ្ឍន៍​...</p>
-      <div>
+      <div className="Profile">
         <div><h3>នឹងជួបគ្នាឆាប់ៗនេះជាមួយ អាត្រា</h3></div>
         <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         <div><img src={profile} alt="profile" /></div>
