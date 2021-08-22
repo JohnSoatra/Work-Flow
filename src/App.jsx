f6cc8bf81575b2c00a6239f9aa0c8dd02c17ff66
John Soatra <johnsoatra@gmail.com>
2021-08-22 16:38:28 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ const fbLink = "https://www.facebook.com/pheareak.nhor";
 const App = () => {
   return (
     <div className="App">
-      <h3>សួរស្ដី​ខ្ញុំបាទឈ្មោះ​ : Pheareak Nhor</h3>
-      <h3>ចំពោះ​ខ្ញុំនៅ : Single</h3>
+      <h3>សួរស្ដី​ខ្ញុំបាទឈ្មោះ​ : <mark><b>Pheareak Nhor</b></mark></h3>
+      <h3>ចំពោះ​ខ្ញុំនៅ : <mark>Single</mark></h3>
       <h3>សិក្សា​នៅ ​​: RUPP</h3>
       <div className="Profile">
-        <div><h4>ចង់ញែ​​អាច​ClickលើLink​​​ រី លើរូបភាពខាងក្រោមបាន!! &#128513;</h4></div>
+        <div><h4>ចង់ញែ​​អាច<mark><b>Click</b></mark>លើLink​​​ រី លើរូបភាពខាងក្រោមបាន!! &#128513;</h4></div>
         <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         <div><a href={fbLink} target="_blank"><img src={profile} alt="profile" /></a></div>
       </div>
-      <a href={ fbLink } target="_blank"><small>Pheareak Nhor</small></a>
+      <a href={ fbLink } target="_blank"><small><mark>Pheareak Nhor</mark></small></a>
       <h3>SEE YOU SOON!!!!</h3>
     </div>
   );
