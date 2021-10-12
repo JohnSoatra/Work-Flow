80c638b75cf6a334319298bd5bd8a18b1d053b47
John Soatra <johnsoatra@gmail.com>
2021-10-12 11:19:51 +0700

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import profile from './img/profile.jpg';
 const fbLink = "https://www.facebook.com/Soatra";
 const name = "អាត្រា";
@@ -16,7 +15,7 @@ const App = () => {
         <div><h4>នេះគឺជាប្រូហ្វាល់របស់ខ្ញុំ...</h4></div>
         <div><h4>តើសង្ហាទេបាត... &#128513;</h4></div>
         <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-        <div><a href={fbLink} target="_blank"><img src={profile} alt="profile" /></a></div>
+        <div><a href={fbLink} target="_blank" rel="noreferrer"><img src={profile} alt="profile" /></a></div>
       </div>
       <h5>សូមចុចលើរូបភាព ដើម្បីទៅកាន់គណនី facebook របស់ខ្ញុំបាត &#128578;</h5>
     </div>
