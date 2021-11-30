d429614503e27494b45ef17b8c2cc460aaec31a6
John Soatra <johnsoatra@gmail.com>
2021-11-30 20:27:07 +0700

    changed

--- workers/index.js
+++ workers/index.js
@@ -15,3 +15,66 @@ $(".product-img").on("click", function(event) {
       break;
   }
 });
+const images = [
+  "airplan.jpg",
+  "a-man.jpg",
+  "dimon.jpg",
+  "hold-liquid.jpg",
+  "carry-color.jpg"
+];
+const colors = [
+  "#8a5c94",
+  "#625e99",
+  "#5d6780",
+  "#be86b9",
+  "#f4b5d4"
+];
+const nameColors = [
+  "#9a0047",
+  "#c1aaf6",
+  "#02425f",
+  "#c1217c",
+  "#ff568b"
+]
+
+let firstTime = true;
+let i = 0;
+let j = 1;
+function backgroundAnimator() {
+  if (!firstTime) {
+    const imageUrl = images[i];
+    const color = colors[i];
+    const nameColor = nameColors[i];
+
+     $(".div-background-second").css({
+       display: "none",
+       backgroundImage: `url(../images/${imageUrl})`
+     });
+     $(".div-background-second").fadeIn(1000, () => {
+       $(".div-background-first").css(
+         "backgroundImage",
+         `url(../images/${imageUrl})`
+        );
+     });
+     $(".header").css("backgroundColor", color);
+     $(".footer").css("borderColor", color + "73");
+     $(".name").css("color", nameColor);
+     $(".starter").css("color", nameColor);
+     $(".product-name").css("color", nameColor);
+  }
+  else {
+    firstTime = false;
+  }
+
+  i += j;
+  if (i === images.length) {
+    i = images.length - 2;
+    j = -1;
+  }
+  else if (i === -1) {
+    i = 1;
+    j = 1;
+  }
+  setTimeout(backgroundAnimator, 8500);
+}
+backgroundAnimator();
