1b8f005e1a5238455d74e970d79587a3956be34a
John Soatra <johnsoatra@gmail.com>
2022-04-05 12:09:52 +0700

    changes

--- workers/index.js
+++ workers/index.js
@@ -1,21 +1,23 @@
 import animatorName from "./animator.js";
 
-document.querySelector(".product-img").addEventListener("click", function(event) {
-  const alt = event.target.alt;
-  switch (alt) {
-    case "posyou":
-      window.open("https://youtube.com/PosYou");
-      break;
-    case "meka":
-      window.open("https://meka.soatra.com");
-      break;
-    case "paperpen":
-      window.open("https://paperpen.soatra.com");
-      break;
-    case "lab":
-      window.open("https://lab.soatra.com");
-      break;
-  }
+document.querySelectorAll(".product-img").forEach(e => {
+  e.addEventListener("click", function(event) {
+    const alt = event.target.alt;
+    switch (alt) {
+      case "posyou":
+        window.open("https://youtube.com/PosYou");
+        break;
+      case "meka":
+        window.open("https://meka.soatra.com");
+        break;
+      case "paperpen":
+        window.open("https://paperpen.soatra.com");
+        break;
+      case "lab":
+        window.open("https://lab.soatra.com");
+        break;
+    }
+  });
 });
 
 document.querySelector(".profile").addEventListener("click", function() {
